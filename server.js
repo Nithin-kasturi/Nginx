fc39487889a2d4476778c40e1dd6fbf2dbfc80ac
Nithin-kasturi <nithinkasturi8@gmail.com>
2025-09-24 21:19:12 +0530

    wrote index and server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const path=require('path');
+const app=express();
+app.listen(3000,()=>{
+    console.log("Listening on port 3000");
+});
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'))
+})
